2abb508141dac6181aa0f1d3266132b19d55432e
Camilo Romero <cromero@yopapp.me>
2023-01-17 11:46:13 -0300

    added hilt to app module

--- app/src/main/java/com/crocdc/solidwaffle/fragments/PokemonListingViewModel.kt
+++ app/src/main/java/com/crocdc/solidwaffle/fragments/PokemonListingViewModel.kt
@@ -1,7 +1,11 @@
 package com.crocdc.solidwaffle.fragments
 
 import androidx.lifecycle.ViewModel
+import com.crocdc.datacore.repos.PokemonRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class PokemonListingViewModel : ViewModel() {
+@HiltViewModel
+class PokemonListingViewModel @Inject constructor(repository: PokemonRepository) : ViewModel() {
     // TODO: Implement the ViewModel
 }
