df373dc4a544a5ae6f54f60570d8a0cb46ba7964
Camilo Romero <cromero@yopapp.me>
2023-01-20 13:14:35 -0300

    improve moves

--- data-networking/src/main/java/com/crocdc/datanetworking/model/Moves.kt
+++ data-networking/src/main/java/com/crocdc/datanetworking/model/Moves.kt
@@ -1,3 +1,8 @@
 package com.crocdc.datanetworking.model
 
-data class Moves(val move: NamedApiResource)
+import com.squareup.moshi.Json
+
+data class Moves(
+    val move: NamedApiResource,
+    @Json(name = "version_group_details") val versionGroupDetails: List<VersionGroupDetails>
+)
