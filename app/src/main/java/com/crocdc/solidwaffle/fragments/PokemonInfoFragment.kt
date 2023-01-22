46155d0637cfdcbe9263af2afe7d735a38fd6628
Camilo Romero <cromero@yopapp.me>
2023-01-22 14:16:25 -0300

    fix error

--- app/src/main/java/com/crocdc/solidwaffle/fragments/PokemonInfoFragment.kt
+++ app/src/main/java/com/crocdc/solidwaffle/fragments/PokemonInfoFragment.kt
@@ -1,7 +1,6 @@
 package com.crocdc.solidwaffle.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.widget.PopupMenu
 import androidx.core.content.ContextCompat
