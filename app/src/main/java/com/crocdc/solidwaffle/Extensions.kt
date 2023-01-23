4f98297235e079b8f842311d5604fcaa892608b8
Camilo Romero <cromero@yopapp.me>
2023-01-23 10:29:41 -0300

    added placeholder.webp

--- app/src/main/java/com/crocdc/solidwaffle/Extensions.kt
+++ app/src/main/java/com/crocdc/solidwaffle/Extensions.kt
@@ -6,5 +6,6 @@ import com.squareup.picasso.Picasso
 fun ImageView.fetchImage(url: String) {
     Picasso.get()
         .load(url)
+        .placeholder(R.drawable.placeholder)
         .into(this)
 }
